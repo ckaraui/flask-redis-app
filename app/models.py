bb25195c0ba58e90f8264ce928316083fdf3039a
ckaraui <61882553+ckaraui@users.noreply.github.com>
2025-12-09 14:35:25 +0100

    Add User and Book models to the application

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
